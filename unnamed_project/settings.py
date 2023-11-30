b3b962e8de18f86c288de7fa9e9cd369017e6b73
peanutmutton <peanutmutton@outlook.com>
2023-11-30 21:43:26 +0600

    ckeditor added

--- unnamed_project/settings.py
+++ unnamed_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     "wiki.apps.WikiConfig",
     #installed apps
+    "ckeditor",
 ]
 
 MIDDLEWARE = [
--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from ckeditor.fields import RichTextField
 
 User = get_user_model()
 class Article(models.Model):
@@ -10,7 +11,7 @@ class Article(models.Model):
 
 class ArticleContent(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
-    html_content = models.TextField()
+    html_content = RichTextField(blank=True, null=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     editor = models.ForeignKey(User, on_delete=models.RESTRICT)
 
